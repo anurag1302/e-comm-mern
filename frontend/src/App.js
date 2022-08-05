f26685fe6ffe8cfa140709f0753e46162d91b4db
Anurag Sinha <anurag3487@gmail.com>
2022-08-05 23:34:23 +0530

    Added Routing Support

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<h1>Home Component</h1>} />
+          <Route path="/products" element={<h1>Products List Component</h1>} />
+          <Route path="/add-product" element={<h1>Add Product Component</h1>} />
+          <Route
+            path="/update-product"
+            element={<h1>Update Product Component</h1>}
+          />
+          <Route path="/logout" element={<h1>Logout Component</h1>} />
+          <Route path="/profile" element={<h1>Profile Component</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/NavBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div>
+      <ul className="navbar-ul">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/products">Products List</Link>
+        </li>
+        <li>
+          <Link to="/add-product">Add Product</Link>
+        </li>
+        <li>
+          <Link to="/update-product">Update Product</Link>
+        </li>
+        <li>
+          <Link to="/logout">Logout</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
